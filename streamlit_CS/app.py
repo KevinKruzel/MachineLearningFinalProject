59c7d3ce8dcb8229275d82794f587b6296c6e788
KevinKruzel <kkruzel1@msudenver.edu>
2025-11-20 22:32:51 -0700

    Update app.py

--- streamlit_CS/app.py
+++ streamlit_CS/app.py
@@ -2,26 +2,44 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Machine Learning Final Project",
-    page_icon="🔴",
+    page_icon="🧭",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="expanded",
 )
 
-st.title("Final Project by Kevin Kruzel")
+st.title("Machine Learning Final Project – Pokémon Type Prediction")
+st.markdown("#### by Kevin Kruzel")
 
 st.markdown(
     """
-    ### 🔴 About this Project
-    
-    This project explores and analyzes...
-    
-    **Pages:**
-    - 🔴 **app** - Overview of the Project
-    - 🔴 **EDA Gallery** - Exploratory data analysis of the dataset, complete with charts and initial observations
-    - 🔴 **Prediction** - Uses machine learning model to predict
-    
-    Navigate using the sidebar to begin exploring the other pages of the project.
+The goal of this project to see how well we can predict a Pokémon’s primary type using only its
+six base stats (HP, Attack, Defense, Special Attack, Special Defense, and Speed) by using a machine learning model.
+
+This app uses interactive tools to explore the dataset and customize the Random Forest model used to predict a Pokémon’s primary type.
+Use the sidebar to filter the dataset to specific Pokémon to explore how these filters affect the distribution of the data and the accuracy of the model.
+)
+
+st.markdown("---")
+
+st.markdown(
     """
+Use the **sidebar** to switch between pages:
+
+- 📊 **EDA Gallery**  
+  Explore the Pokémon dataset with interactive visualizations.  
+  - View a type heatmap and bar chart to see the distribution of primary and secondary types of Pokemon
+  - View a series of boxplots to visualize the differences in distribution of Pokémon stats grouped by type.
+  - Create a fully customizable scatterplot to explore the distribution of two select stats for all Pokémon in select type groups.
+
+- 🤖 **Machine Learning Model**  
+  Build and evaluate a Random Forest model that predicts a Pokémon’s primary type.  
+  - Adjust model hyperparameters (number of trees, depth, max features, etc.).
+  - View a confusion matrix showing exactly what predictions the model is making.
+  - See feature importance in a bar chart to understand which stats matter most.
+  - Inspect an example decision tree from the forest to better understand the model.
+
+Head over to the other pages in the sidebar to start exploring.
+"""
 )
 
 st.caption("Built with Streamlit")
